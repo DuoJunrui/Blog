5ed902cab1a1a7c27760355812bebcd456d37deb
Duojunrui <duojunrui@163.com>
2019-08-17 17:41:31 +0800

    日志处理

--- src/main/java/cn/duojunrui/blog/web/IndexController.java
+++ src/main/java/cn/duojunrui/blog/web/IndexController.java
@@ -3,6 +3,7 @@ package cn.duojunrui.blog.web;
 import cn.duojunrui.blog.exception.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @Author: Duojunrui
@@ -11,13 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable String id, @PathVariable String name) {
 //        int i = 9/0;
-        String blog = null;
-        if (blog == null) {
-            throw new NotFoundException("博客不存在");
-        }
+//        String blog = null;
+//        if (blog == null) {
+//            throw new NotFoundException("博客不存在");
+//        }
+
+        System.out.println("--------index-----------");
         return "index";
     }
 }
