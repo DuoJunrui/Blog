c3846861eb710ab60fd000e2c439ebab4817679a
Duojunrui <duojunrui@163.com>
2019-08-19 00:40:18 +0800

    编码规约-常量应使用全大写字母拼写

--- src/main/java/cn/duojunrui/blog/util/MD5Utils.java
+++ src/main/java/cn/duojunrui/blog/util/MD5Utils.java
@@ -12,15 +12,16 @@ public class MD5Utils {
     /**
      * 盐值，用于混交md5
      */
-    private static final String slat = "181!@#$..2520";
+    private static final String SALT = "181!@#$..2520";
 
     /**
      * 生成md5，这里使用Spring自带的DigestUtils工具类
+     *
      * @param str
      * @return md5
      */
     public static String getMD5(String str) {
-        String base = str +"/"+slat;
+        String base = str + "/" + SALT;
         String md5 = DigestUtils.md5DigestAsHex(base.getBytes());
         return md5;
     }
