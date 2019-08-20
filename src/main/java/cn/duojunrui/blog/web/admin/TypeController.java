13a1c06990583b6e291eb1569d1894bb55f8c36b
Duojunrui <duojunrui@163.com>
2019-08-20 23:49:13 +0800

    管理后台-分类管理(二)，新增及分页

--- src/main/java/cn/duojunrui/blog/web/admin/TypeController.java
+++ src/main/java/cn/duojunrui/blog/web/admin/TypeController.java
@@ -1,5 +1,6 @@
 package cn.duojunrui.blog.web.admin;
 
+import cn.duojunrui.blog.entity.Type;
 import cn.duojunrui.blog.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -21,12 +23,26 @@ public class TypeController {
     @Autowired
     private TypeService typeService;
 
-    @GetMapping("types")
-    public String types(@PageableDefault(size = 10, sort = {"id"}, direction = Sort.Direction.DESC)
+    @GetMapping("/types")
+    public String types(@PageableDefault(size = 3, sort = {"id"}, direction = Sort.Direction.DESC)
                                 Pageable pageable, Model model) {
-
         model.addAttribute("page", typeService.listType(pageable));
-
         return "/admin/types";
+    }
+
+    @GetMapping("/types/add")
+    public String add() {
+        return "/admin/types_add";
+    }
+
+    @PostMapping("/types")
+    public String save(Type type) {
+        Type newType = typeService.saveType(type);
+        if (newType == null) {
+
+        } else {
+
+        }
+        return "redirect:/admin/types";
     }
 }
